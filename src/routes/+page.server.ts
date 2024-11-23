3ead67875d3f8f4931f8328fd087edfc3ec31b4d
Nicolae Rusu <nicolae@rusu.dev>
2024-11-23 15:29:02 -0500

    add: get stores with active flyers

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,30 +1,43 @@
 import { fail, type Actions } from '@sveltejs/kit';
 
-const ENDPOINT_FLYERS_BY_POSTAL_CODE = `https://flyers-ng.flippback.com/api/flipp/data`;
+const ENDPOINT_FLYERS_BY_POSTAL_CODE = (postalCode: string) =>
+	`https://flyers-ng.flippback.com/api/flipp/data?postal_code=${postalCode}&sid=${Math.random().toString().slice(2, 10)}`;
+
+const ENDPOINT_FLYER_ITEMS = (flyerId: number) =>
+	`https://flyers-ng.flippback.com/api/flipp/flyers/${flyerId}/flyer_items`;
 
 export const actions = {
-	default: async ({ request }) => {
+	getFlyers: async ({ request }) => {
 		const data = await request.formData();
 		const postalCode = data.get('postalCode') as string;
-		console.log(data);
 		const flyers = await getFlyersByPostalCode(postalCode);
-		console.log(flyers);
-		// if (flyers.error) {
-		// 	fail(flyers.error);
 
-		// 		}
-				return postalCode;
+		return { stores: flyers };
+	},
+	getRecipes: async ({ request }) => {
+		const data = await request.formData();
+		return { status: 200, body: { recipes: [] } };
+		// const flyerItems = [];
+		// flyers.forEach((flyer) => flyerItems.push(getFlyerItemsByFlyerId(flyer.id)));
 	}
 } satisfies Actions;
 
-
 const getFlyersByPostalCode = async (postalCode: string) => {
-	const sid = Math.random().toString().slice(2, 10);
-	const response = await fetch(`${ENDPOINT_FLYERS_BY_POSTAL_CODE}?postal_code=${postalCode}&sid=${sid}`);
+	const response = await fetch(ENDPOINT_FLYERS_BY_POSTAL_CODE(postalCode));
 	const data = await response.json();
-	if (data.error) 
-	console.log(data);
-	// return data;
-	// return postalCode;
-	
-}
+	const currentDate = new Date().toISOString().slice(0, 10);
+	const flyers = data.flyers.filter((flyer) => {
+		return (
+			(flyer.categories.includes('Épicerie') || flyer.categories.includes('Groceries')) &&
+			flyer.valid_from.slice(0, 10) <= currentDate &&
+			flyer.valid_to.slice(0, 10) >= currentDate
+		);
+	});
+	return flyers;
+};
+
+const getFlyerItemsByFlyerId = async (flyerId: number) => {
+	const response = await fetch(ENDPOINT_FLYER_ITEMS(flyerId));
+	const data = await response.json();
+	return data;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,24 +2,43 @@
 	let { form }: { form: ActionData } = $props();
 	import { enhance } from '$app/forms';
 	import { Input } from '$lib/components/ui/input';
-	import type { ActionData } from './$types'
+	import type { ActionData } from './$types';
 	import { onMount } from 'svelte';
+	import { Checkbox } from '$lib/components/ui/checkbox';
+	import { Label } from '$lib/components/ui/label';
 	let postalCode = $state('');
+	let selectedStores = $state<string[]>([]);
+	$effect(() => {
+		console.log('form:', form);
+	});
+
+	const addStore = (store) => {
+		selectedStores = [...selectedStores, store];
+	};
+
+	const removeStore = (store) => {
+		selectedStores = selectedStores.filter((s) => s !== store);
+	};
 	onMount(() => {
 		console.log('mounted');
 		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(async (position) => {
-			const { latitude, longitude } = position.coords;
-			const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`);
-			const data = await response.json();
-			console.log(data);
-			console.log(data.results[0].components.postcode);
-			if (data.results && data.results.length > 0) {
-				postalCode = data.results[0].components.postcode || '';
-			}
-			}, (error) => {
-			console.error('Error getting location:', error);
-			});
+			navigator.geolocation.getCurrentPosition(
+				async (position) => {
+					const { latitude, longitude } = position.coords;
+					const response = await fetch(
+						`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`
+					);
+					const data = await response.json();
+					console.log(data);
+					console.log(data.results[0].components.postcode);
+					if (data.results && data.results.length > 0) {
+						postalCode = data.results[0].components.postcode || '';
+					}
+				},
+				(error) => {
+					console.error('Error getting location:', error);
+				}
+			);
 		} else {
 			console.error('Geolocation is not supported by this browser.');
 		}
@@ -31,8 +50,31 @@
 
 	<h2>Tagline here</h2>
 	<p>Enter your postal code :3</p>
-	<form method="POST" use:enhance>
+	<form method="POST" use:enhance action="?/getFlyers">
 		<Input name="postalCode" value={postalCode} class="text-center" placeholder="H1A B2C" />
 	</form>
-	{form}
+	{#if form?.stores}
+		{#each [...new Map(form.stores.map((store) => [store.merchant, store])).values()] as store}
+			{@const checked = selectedStores.includes(store.merchant)}
+			<div>
+				<Checkbox
+					id={store.merchant}
+					{checked}
+					value={store.merchant}
+					onCheckedChange={(v) => {
+						if (v) {
+							addStore(store.merchant);
+						} else {
+							removeStore(store.merchant);
+						}
+					}}
+				/>
+				<Label
+					for={store.merchant}
+					class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+					>{store.merchant}</Label
+				>
+			</div>
+		{/each}
+	{/if}
 </section>
