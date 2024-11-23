16f0d477ce1102147a347cf86cd56fc9d40f4a7c
Nicolae Rusu <nicolae@rusu.dev>
2024-11-23 15:32:08 -0500

    inverse conditionals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,28 +20,26 @@
 		selectedStores = selectedStores.filter((s) => s !== store);
 	};
 	onMount(() => {
-		console.log('mounted');
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(
-				async (position) => {
-					const { latitude, longitude } = position.coords;
-					const response = await fetch(
-						`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`
-					);
-					const data = await response.json();
-					console.log(data);
-					console.log(data.results[0].components.postcode);
-					if (data.results && data.results.length > 0) {
-						postalCode = data.results[0].components.postcode || '';
-					}
-				},
-				(error) => {
-					console.error('Error getting location:', error);
-				}
-			);
-		} else {
+		if (!navigator.geolocation) {
 			console.error('Geolocation is not supported by this browser.');
+			return;
 		}
+		navigator.geolocation.getCurrentPosition(
+			async (position) => {
+				const { latitude, longitude } = position.coords;
+				const response = await fetch(
+					`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`
+				);
+				const data = await response.json();
+
+				if (data.results && data.results.length > 0) {
+					postalCode = data.results[0].components.postcode || '';
+				}
+			},
+			(err) => {
+				console.error('Error getting location:', err);
+			}
+		);
 	});
 </script>
 
