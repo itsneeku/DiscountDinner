f08d1d2390cf5cf40f1497c6c7f497e44837655d
Nicolae Rusu <nicolae@rusu.dev>
2024-11-23 14:22:24 -0500

    fix: https on dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 import dotenv from 'dotenv';
 dotenv.config();
 export default defineConfig({
-	plugins: [sveltekit()],
-	
+	plugins: [sveltekit(), mkcert()],
+	server: { https: true }
 });
