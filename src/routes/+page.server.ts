8e72f7ce580ca63580bdc550d9d99d4dd7a4325b
Dany Pham <96401568+DanyPDev@users.noreply.github.com>
2024-11-23 18:46:32 +0000

    add geolocated postal code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,30 @@
-import type { Actions } from '@sveltejs/kit';
+import { fail, type Actions } from '@sveltejs/kit';
+
+const ENDPOINT_FLYERS_BY_POSTAL_CODE = `https://flyers-ng.flippback.com/api/flipp/data`;
 
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
-		const postalCode = data.get('postalCode');
+		const postalCode = data.get('postalCode') as string;
 		console.log(data);
-		return postalCode;
+		const flyers = await getFlyersByPostalCode(postalCode);
+		console.log(flyers);
+		// if (flyers.error) {
+		// 	fail(flyers.error);
+
+		// 		}
+				return postalCode;
 	}
 } satisfies Actions;
+
+
+const getFlyersByPostalCode = async (postalCode: string) => {
+	const sid = Math.random().toString().slice(2, 10);
+	const response = await fetch(`${ENDPOINT_FLYERS_BY_POSTAL_CODE}?postal_code=${postalCode}&sid=${sid}`);
+	const data = await response.json();
+	if (data.error) 
+	console.log(data);
+	// return data;
+	// return postalCode;
+	
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,28 @@
 	let { form }: { form: ActionData } = $props();
 	import { enhance } from '$app/forms';
 	import { Input } from '$lib/components/ui/input';
-	import type { ActionData } from './sverdle/$types';
+	import type { ActionData } from './$types'
+	import { onMount } from 'svelte';
+	let postalCode = $state('');
+	onMount(() => {
+		console.log('mounted');
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(async (position) => {
+			const { latitude, longitude } = position.coords;
+			const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`);
+			const data = await response.json();
+			console.log(data);
+			console.log(data.results[0].components.postcode);
+			if (data.results && data.results.length > 0) {
+				postalCode = data.results[0].components.postcode || '';
+			}
+			}, (error) => {
+			console.error('Error getting location:', error);
+			});
+		} else {
+			console.error('Geolocation is not supported by this browser.');
+		}
+	});
 </script>
 
 <section class="flex flex-col justify-center items-center flex-[0.6]">
@@ -11,7 +32,7 @@
 	<h2>Tagline here</h2>
 	<p>Enter your postal code :3</p>
 	<form method="POST" use:enhance>
-		<Input name="postalCode" class="text-center" placeholder="H1A B2C" />
+		<Input name="postalCode" value={postalCode} class="text-center" placeholder="H1A B2C" />
 	</form>
 	{form}
 </section>
